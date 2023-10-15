37518fd815834200e8727fdf50718ebbd8144dae
RoryGopsill <108179552+RoryGopsill@users.noreply.github.com>
2023-10-15 15:19:17 +0100

    Add files via upload

---
+++ Data Science course 2 final assignment.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science tools and ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# - Add comments to code cell
+# - Create markdown cells
+# - Add ordered and unordered lists to markdown cells
+# - Format content of markdown cells with heading styles
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. NumPy
+# 3. Keras
+
+# Some of the popular languages that data scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. C
+
+# |Data Science Tools|
+# |------------------|
+# |RStudio|
+# |Anaconda|
+# |Spyder|
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+minutes = int(input("Please enter the number of minutes: "))
+hours = minutes/60
+print(hours)
+
+
+# ## Author
+# Rory Gopsill
+
+# In[ ]:
+
+
+
+
